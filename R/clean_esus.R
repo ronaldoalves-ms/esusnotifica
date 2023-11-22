626f11b219f0e1e4b8bc7b19fe2cd5a9c569557d
Cid de Paulo Felipe dos Santos <cid.paulo@gmail.com>
2023-11-22 16:43:48 -0300

    Update clean_esus.R
    ajustando nomesVars

--- R/clean_esus.R
+++ R/clean_esus.R
@@ -35,7 +35,7 @@ clean_esus <- function(dados){
   # Validando o nome das variáveis para evitar carregamento com "X." ou "X_" por exemplo "X_update_at" vai ficar "update_at"
   # Validando o nome de variáveis que iniciam com _ para evitar erro, removendo o _ exemplo _update_at fica update_at
 
-  if (any(grepl("^[_|@]|^X[_|.]",namesVars))){
+  if (any(grepl("^[_|@]|^X[_|.]",nomesVars))){
     names(dados) <- gsub(nomesVars,pattern ="^[_|@]|^X[_|.]", replacement = "", ignore.case = T)
     
   } 
