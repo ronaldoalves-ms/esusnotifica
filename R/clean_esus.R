f30da5df9a30fd6d42db48c3e376b1b5f02b633d
Cid de Paulo Felipe dos Santos <cid.paulo@gmail.com>
2023-11-22 16:48:11 -0300

    Update clean_esus.R
    Correção log de dataAtualização

--- R/clean_esus.R
+++ R/clean_esus.R
@@ -182,7 +182,7 @@ clean_esus <- function(dados){
     dados$dataRegistro[dados$dataRegistro > Sys.Date()] <- NA
     
   }
-  print(names(dados) == "dataRegistro", 'Data Registro encontrada!')
+  print(paste(names(dados) == "dataRegistro", 'Data Registro encontrada!'))
   
   # DATA DA ATUALIZACAO DO REGISTRO
   
@@ -197,7 +197,7 @@ clean_esus <- function(dados){
     
   }
   
-  print(names(dados) == "dataAtualizacao", 'Data atualizacao encontrada!')
+  print(paste(names(dados) == "dataAtualizacao", 'Data atualizacao encontrada!'))
   
   print('Tratamento das datas concluido')
   
